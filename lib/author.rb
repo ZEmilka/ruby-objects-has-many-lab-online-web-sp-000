4f50a8159ffab8f460bcfc5af79440d1b7f152f9
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-03 21:02:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 
   attr_accessor :name, :posts
-  @@count=0
+  @@post_count=0
 
   def initialize(name)
     @name=name
@@ -11,17 +11,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@count += 1
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
-    @@count += 1
+    @@post_count += 1
   end
 
   def self.post_count
-    @@count
+    @@post_count
   end
 end
